4994d1974dd6b0a520edb1056ce8a8f533d19b3e
Rayan Kazi <rayankazi7515@gmail.com>
2025-06-19 17:12:40 -0700

    moved start page to different file

--- square-ai/components/convex-client-provider.tsx
+++ square-ai/components/convex-client-provider.tsx
@@ -4,13 +4,10 @@ import { ReactNode } from "react";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { useAuth } from "@clerk/nextjs";
+
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-export default function ConvexClientProvider({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export function ConvexClientProvider({ children }: { children: ReactNode }) {
   return (
     <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
       {children}
