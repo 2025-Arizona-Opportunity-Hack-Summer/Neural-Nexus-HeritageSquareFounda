b5fbbc3a74f1f603f514622ad1a15cc34f39f050
Rayan Kazi <rayankazi7515@gmail.com>
2025-06-19 17:18:15 -0700

    added app sidebar

--- square-ai/app/page.tsx
+++ square-ai/app/page.tsx
@@ -1,5 +1,45 @@
 "use client";
 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+// import { ChatInput } from "@/components/chat-input";
+import { SidebarTrigger, WithSidebarLayout } from "@/components/app-sidebar";
+
 export default function Home() {
-  return <div>Hi</div>;
+  return (
+    <WithSidebarLayout>
+      <div className="flex h-screen">
+        {/* Main Content Area */}
+        <div className="flex-1 flex flex-col min-w-0">
+          {/* Header */}
+          <header className="flex items-center justify-between w-full flex-shrink-0 p-4">
+            <SidebarTrigger />
+
+            <Avatar>
+              <AvatarImage
+                src="https://github.com/shadcn.png"
+                alt="User Avatar"
+              />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </header>
+
+          {/* What's up page */}
+
+          <main className="flex-grow flex flex-col items-center justify-center w-full">
+            <div className="text-center">
+              <p className="text-5xl font-bold text-gray-700">Ask away!</p>
+              <p className="mt-2 text-gray-400">
+                I&apos;m here to help you with anything.
+              </p>
+            </div>
+          </main>
+
+          {/* Chat Input at the bottom */}
+          <footer className="w-full flex-shrink-0 flex justify-center p-4">
+            {/* <ChatInput /> */}
+          </footer>
+        </div>
+      </div>
+    </WithSidebarLayout>
+  );
 }
---
+++ square-ai/components/app-sidebar.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import * as React from "react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+  SidebarTrigger as DefaultSidebarTrigger,
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
+import { Button } from "@/components/ui/button";
+import { Pin, X } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+const newData = {
+  chats: [
+    { title: "Ancient civilization interest", url: "#" },
+    { title: "Modern interests", url: "#" },
+    {
+      title:
+        "Ancient civilization interestinterestinterestinterestinterestinterest",
+      url: "#",
+    },
+    { title: "Ancient civilization interests", url: "#" },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#" className="transition-all duration-200">
+                <div className="flex flex-col items-center gap-0.5 leading-none">
+                  <span className="font-bold text-center text-2xl">
+                    shareable.ai
+                  </span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+
+          <SidebarMenuItem>
+            <Button variant="default" className="cursor-pointer w-full">
+              <span className="text-sm">New Chat</span>
+            </Button>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Chats</SidebarGroupLabel>
+          <SidebarMenu>
+            {newData.chats.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <div className="group/item flex w-full items-center justify-start overflow-hidden cursor-pointer">
+                    <a
+                      href={item.url}
+                      className="truncate duration-200 flex-grow text-gray-600"
+                    >
+                      {item.title}
+                    </a>
+
+                    <Pin className="size-5 flex-shrink-0 opacity-0 translate-x-5 group-hover/item:opacity-100 group-hover/item:translate-x-0 transition-all duration-300 ease-in-out cursor-pointer hover:text-black" />
+
+                    <X className="size-5 flex-shrink-0 opacity-0 translate-x-5 group-hover/item:opacity-100 group-hover/item:translate-x-0 transition-all duration-300 ease-in-out cursor-pointer hover:text-black" />
+                  </div>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
+
+export function SidebarTrigger() {
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <DefaultSidebarTrigger className="cursor-pointer" />
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>⌘B</p>
+      </TooltipContent>
+    </Tooltip>
+  );
+}
+
+export function WithSidebarLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>{children}</SidebarInset>
+    </SidebarProvider>
+  );
+}
