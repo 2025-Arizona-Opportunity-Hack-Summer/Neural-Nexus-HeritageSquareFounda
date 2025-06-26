ae39e85922d8e7b5b9cae1264842601c4ff3317f
Doctor-Schultz <114769197+Doctor-Schultz@users.noreply.github.com>
2025-06-26 11:59:00 -0700

    Delete irrelevant files from main
    This side project does not share any of the same files as the main branch, however for team coherency we are saving this project as a branch.

--- square-ai/app/admin/page.tsx
+++
@@ -1,7 +0,0 @@
-import Component from "@/components/admin-panel";
-import React from "react";
-
-export default function AdminPage() {
-  // Add verification for if user is admin or not
-  return <Component />;
-}
